3987e1121e376ccd34955fca37df3026f37dc221
Davi <roxdavirox@gmail.com>
2023-04-29 23:12:58 -0300

    feat: add patch partial update

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -23,6 +23,7 @@ router.get('/categories', categoryController.getAllCategories);
 router.get('/categories/:id', categoryController.getCategoryById);
 router.post('/categories', categoryController.createCategory);
 router.put('/categories/:id', categoryController.updateCategory);
+router.patch('/categories/:id', categoryController.partialUpdateCategory);
 router.delete('/categories/:id', categoryController.deleteCategory);
 
 module.exports = router;
